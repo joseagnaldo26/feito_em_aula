8f2f072940f72757f16ad22fd838c561711bcee8
joseagnaldo26 <jose.agnaldo@pucpr.edu.br>
2024-06-07 20:33:54 -0300

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,23 @@
+import pandas as pd  
+import matplotlib.pyplot as plt
+import streamlit as st
+
+st.header("Dados da empresa 1")
+
+arquivo = "https://raw.githubusercontent.com/joseagnaldo26/feito_em_aula/main/empresa1.csv" 
+dfe = pd.read_csv(arquivo, sep=';') 
+st.dataframe(dfe) 
+
+fig, ax = plt.subplots()
+dfe.plot()
+st.pyplot(fig)
+
+fig, ax = plt.subplots()
+dfe.plot(kind = 'scatter', x = 'EBITDA', y = 'Lucro operacional')
+st.pyplot(fig)
+
+fig, ax = plt.subplots()
+dfe["Lucro do período"].plot(kind = 'hist')
+st.pyplot(fig)
+
+st.write(dfe.groupby('Ano').mean())
